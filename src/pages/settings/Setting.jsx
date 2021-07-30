a406b2d34b33d4e1266b1e6a58cbffbdc25e2222
agarwal1999 <agarwal18b@iitg.ac.in>
2021-07-30 15:04:29 +0530

    user settings page added

---
+++ src/pages/settings/Setting.jsx
@@ -0,0 +1,35 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import "./setting.css"
+
+const Setting = () => {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+              <input type="file" id="fileInput" style={{display:"none"}} />
+            </label>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Saurabh" />
+          <label>Email</label>
+          <input type="email" placeholder="saurabh@gmail.com" />
+          <label>Password</label>
+          <input type="password"/>
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Setting
