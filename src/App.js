c0e6c5dfded2db4b2959ddace1489b4aba178ac9
agarwal1999 <agarwal18b@iitg.ac.in>
2021-07-30 21:51:17 +0530

    added register and login page and react routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <TopBar/>
-      <Setting/>
-    </>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login">
+        {user ? <Home/> : <Login/>}
+        </Route>
+        <Route exact path="/register">
+          {user ? <Home/> : <Register/>}
+        </Route>
+        <Route exact path="/write">
+          {user ? <Write/> : <Register/>}
+        </Route>
+        <Route exact path="/settings">
+        {user ? <Setting/> : <Register/>}
+        </Route>
+        <Route exact path="/post/:postId" component={Single} />
+      </Switch>
+    </Router>
   );
 }
 
