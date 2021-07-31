be6e873e53a60532850d9aa6d9dcacae307f6c62
agarwal1999 <agarwal18b@iitg.ac.in>
2021-07-31 22:27:52 +0530

    added crud oprations for users

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 app.use(express.json());
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 .catch(err=>console.log(err));
 
 app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute);
 
 
 app.listen("5000", ()=>{
