c5d9e4e13f87bec6c23bfc11279e837123058ad1
agarwal1999 <agarwal18b@iitg.ac.in>
2021-07-31 17:47:30 +0530

    added register new user and login functionality in api

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
 .then(console.log("Connected to MongoDB"))
 .catch(err=>console.log(err));
+
+app.use("/api/auth", authRoute)
 
 
 app.listen("5000", ()=>{
