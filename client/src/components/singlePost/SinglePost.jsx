da9fc1fcd16364b9f34682c9ba6b3f49e6ec0e4d
agarwal1999 <agarwal18b@iitg.ac.in>
2021-08-05 19:50:32 +0530

    deleting a post

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom"
+import {Context} from "../../context/Context";
 import "./singlePost.css"
 
 const SinglePost = () => {
   const location = useLocation();
   const id = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const { user } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
 
   useEffect(()=>{
     const getPost = async ()=>{
@@ -15,17 +18,25 @@ const SinglePost = () => {
     }
     getPost();
   },[id]);
-  const PF = "http://localhost:5000/images/"
+
+  const handleDelete = async ()=>{
+    await axios.delete(`http://localhost:5000/api/posts/delete/${id}`, {data:{username:user.username}})
+    .then(res=>window.location.replace("/"))
+    .catch(err=>console.log(err))
+  }
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
         {post.photo && (<img src={PF + post.photo} alt="" className="singlePostImg" />)}
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
+          {post.username===user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
